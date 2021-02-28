831f79e4d58ee9a8210fd61eb1b49209e4420b46
Arsalan <arsheikh665@gmail.com>
2021-03-01 04:11:00 +0500

    updated verification and login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { InputLabel, Input, InputAdornment, IconButton, Button, FormControl } from '@material-ui/core/';
-import {Visibility, VisibilityOff} from '@material-ui/icons';
-import {Link} from 'react-router-dom';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 import "./login.css"
-import { firebaseConfig as firebase} from "../Configs/fbsConfig"
+import { firebaseConfig as firebase } from "../Configs/fbsConfig"
 import "firebase/auth";
 
-
-const UserLogin = (userValues) => (e) => {
-  e.preventDefault();
-  console.log(userValues.email, userValues.password)
-  firebase.auth().signInWithEmailAndPassword(userValues.email, userValues.password)
-  .then((userCredential) => {
-    console.log("Done: ", userCredential)
-  })
-  .catch((error) => {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
-}
 
 const useStyles = makeStyles((theme) => ({
   mainRoot: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    flexWrap:"wrap",
+    flexWrap: "wrap",
     marginTop: 40,
     padding: 40
   },
@@ -42,13 +26,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function LoginForm() {
   const classes = useStyles();
+  const [lblError, setLblError] = React.useState("");
   const [values, setValues] = React.useState({
     email: '',
     password: '',
     showPassword: false,
   });
   
-  document.title="Login";
+  document.title = "Login";
   const handleChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
@@ -60,11 +45,48 @@ export default function LoginForm() {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+  
+const UserLogin = (userValues) => (e) => {
+  e.preventDefault();
+  console.log(userValues.email, userValues.password)
+  firebase.auth().signInWithEmailAndPassword(userValues.email, userValues.password)
+    .then((userCredential) => {
+
+      var starCountRef = firebase.database().ref(`storeUsers/user${userCredential.user.uid}/verified`)
+      
+      starCountRef.on('value', function (getVerified) {
+
+        const getVerification = getVerified.val();
+
+        if (getVerification === true) {
+          setLblError("Login Successfully")
+        }
+        else if(getVerification === false){
+          setLblError("Account is not Verified !! Wait ....")
+          window.location.replace("/VerificationPage")
+        }
+    })
+
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      console.log(errorCode)
+      console.log(errorMessage)
+      if(errorCode === "auth/user-not-found"){
+      setLblError("No such User in the record !")
+      }
+      else if("The password is invalid or the user does not have a password."){
+        setLblError("Password is invalid")
+      }
+    });
+}
   return (
     <div className={classes.mainRoot}>
       <div className="loginForm">
-      <h3>Login to site</h3>
-      <form className={classes.root} autoComplete="off" onSubmit={UserLogin(values)}>
+        <h3>Login to site</h3>
+        <form className={classes.root} onSubmit={UserLogin(values)}>
           <FormControl>
             <InputLabel htmlFor="my-input">Email address</InputLabel>
             <Input
@@ -75,36 +97,39 @@ export default function LoginForm() {
               onChange={handleChange('email')}
             />
           </FormControl>
-          <br/>
+          <br />
           <FormControl>
             <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
             <Input
-                required
-                id="standard-adornment-password"
-                type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={handleChange('password')}
-                endAdornment={
+              required
+              id="standard-adornment-password"
+              type={values.showPassword ? 'text' : 'password'}
+              value={values.password}
+              onChange={handleChange('password')}
+              endAdornment={
                 <InputAdornment position="end">
-                    <IconButton
+                  <IconButton
                     aria-label="toggle password visibility"
                     onClick={handleClickShowPassword}
                     onMouseDown={handleMouseDownPassword}
-                    >
+                  >
                     {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
+                  </IconButton>
                 </InputAdornment>
-                }
+              }
             />
-          </FormControl><br/>
+          </FormControl><br />
           <Button type="Submit" variant="contained" color="primary">Login</Button>
-      </form>
-      
-      <Button size="small" type="button" color="primary" style={{ textTransform: "lowercase"}}>Forgot Password</Button>
-      <br/><Link to="/Signup" style={{textDecoration:"none"}}>
-        <Button size="small" type="button" variant="outlined" color="secondary">Not have any account</Button>
-      </Link>
-    </div>
+        </form>
+
+        <Button size="small" type="button" color="primary" style={{ textTransform: "lowercase" }}>Forgot Password</Button>
+        <br />
+        <Link to="/Signup" style={{ textDecoration: "none" }}>
+          <Button size="small" type="button" variant="outlined" color="secondary">Not have any account</Button>
+        </Link>
+        <br/>
+      <span style={{color: "red"}}>{lblError}</span>
+      </div>
     </div>
   );
 }
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -29,6 +29,7 @@ export default function Signup() {
   const [refForm, setRefForm] = React.useState();
   const [formDisabler, setFormDisabler] = React.useState("inline-block");
   const [vfDisabler, setVfDisabler] = React.useState("hidden");
+  const [lblError, setLblError] = React.useState("");
   const [userValues, setUserValues] = React.useState({
     username: '',
     email: "",
@@ -41,76 +42,48 @@ export default function Signup() {
 
   function emailLinkComplete() {
     if (firebaseConfig.auth().isSignInWithEmailLink(window.location.href)) {
-      // Additional state parameters can also be passed via URL.
-      // This can be used to continue the user's intended action before triggering
-      // the sign-in operation.
-      // Get the email if available. This should be available if the user completes
-      // the flow on the same device where they started it.
       var email = window.localStorage.getItem('emailForSignIn');
-      var username = window.localStorage.getItem('usernameSignIn');
+      var password = window.localStorage.getItem('passwordSignIn');
       var verified = window.localStorage.getItem('verificationSignIn');
       if (!email) {
-        // User opened the link on a different device. To prevent session fixation
-        // attacks, ask the user to provide the associated email again. For example:
-        // email = window.prompt('Please provide your email for confirmation');
-        
         setFormDisabler("none")
         setVfDisabler("visible")
         setRefForm(
-          <div style={{visibility: formDisabler}}>
+          <div style={{ visibility: formDisabler }}>
             <h4>Please Verify Your Email Address to Process</h4>
             <button onClick={sendingVerification}>Verify Email</button>
           </div>
         )
       }
-      //arsheikh665@gmail.com
-      else{
-        firebaseConfig.database().ref('/storeUsers/' + (username)+"/").update({verified:true})
-        .then(()=> {
-          window.location.replace("/login")
-          
-          window.localStorage.removeItem("emailForSignIn");
-          window.localStorage.removeItem("usernameSignIn");
-          window.localStorage.removeItem("verificationSignIn");
-        })
-        .catch((err)=>{
-          console.log(err)
-          console.log(err)
-        })
-        console.log(username, verified)
-        
+      else {
+        firebaseConfig.auth().signInWithEmailAndPassword(email, password)
+          .then((userCredential) => {
+            firebaseConfig.database().ref(`storeUsers/user${userCredential.user.uid}`).update(
+              { verified: true, id: userCredential.user.uid })
+              .then(() => {
+                window.location.replace("/login")
+
+                window.localStorage.removeItem("emailForSignIn");
+                window.localStorage.removeItem("passwordSignIn");
+                window.localStorage.removeItem("verificationSignIn");
+              })
+              .catch((err) => {
+                console.log(err)
+                console.log(err)
+              })
+          })
       }
-
-      // firebaseConfig.auth().signInWithEmailLink(email, window.location.href)
-      //   .then((result) => {
-      //     // Clear email from storage.
-      //     window.localStorage.removeItem('emailForSignIn');
-
-      //     console.log(result)
-      //   })
-      //   .catch((error) => {
-      //     // Some error occurred, you can inspect the code: error.code
-      //     // Common errors could be invalid email and invalid or expired OTPs.
-      //     console.log(error)
-      //   });
     }
   }
   function sendingVerification() {
     var actionCodeSettings = {
-      // URL you want to redirect back to. The domain (www.example.com) for this
-      // URL must be in the authorized domains list in the Firebase Console.
       url: 'http://localhost:3000/Signup',
-      // This must be true.
       handleCodeInApp: true,
     };
-    console.log(firebaseConfig.auth())
     firebaseConfig.auth().sendSignInLinkToEmail(userValues.email, actionCodeSettings)
       .then(() => {
-        // The link was successfully sent. Inform the user.
-        // Save the email locally so you don't need to ask the user for it again
-        // if they open the link on the same device.
         window.localStorage.setItem('emailForSignIn', userValues.email);
-        window.localStorage.setItem('usernameSignIn', userValues.username);
+        window.localStorage.setItem('passwordSignIn', userValues.password);
         window.localStorage.setItem('verificationSignIn', userValues.verified);
         alert("Email has been send to email address")
         // ...
@@ -126,26 +99,36 @@ export default function Signup() {
   const UserRegisteration = (e) => {
     e.preventDefault();
 
-    firebaseConfig.auth().createUserWithEmailAndPassword(userValues.email, userValues.password)
+    if (userValues.confPass === "") {
+
+    }
+    else if (userValues.password !== userValues.confPass) {
+      setLblError("Password Doesnot match!")
+    }
+    else if (userValues.password === userValues.confPass) {
+
+    const AddingToDB=()=>{
+      firebaseConfig.auth().createUserWithEmailAndPassword(userValues.email, userValues.password)
       .then((userCredential) => {
         firebaseConfig.database().ref('/storeUsers/user' + (userCredential.user.uid)).update(userValues)
           .then((result) => {
             setFormDisabler("none")
             setVfDisabler("visible")
             setRefForm(
-              <div style={{visibility: formDisabler}}>
-              <h4>Please Verify Your Email Address to Process</h4>
+              <div style={{ visibility: formDisabler }}>
+                <h4>Please Verify Your Email Address to Process</h4>
                 <button onClick={sendingVerification}>Verify Email</button>
               </div>
             )
             // sendingVerification()
-            
+
           })
           .catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log(errorCode)
-            console.log(errorMessage)
+            setLblError(errorMessage)
+
           });
       })
       .catch((error) => {
@@ -153,33 +136,43 @@ export default function Signup() {
         var errorMessage = error.message;
         console.log(errorCode)
         console.log(errorMessage)
+        setLblError(errorMessage)
       });
+    }
+      firebaseConfig.database().ref('storeUsers').orderByChild('username').equalTo(userValues.username).on("value", function (snapshot) {
+        if(snapshot.val() === null){
+        console.log(snapshot.val())
+        AddingToDB()
+        }
+        else {
+        setLblError(`Username Exist, Please try another`);
+          return
+        }
+        return
+      })
+      
+    }
   }
-    const handleChange = (prop) => (event) => {
-      setUserValues({ ...userValues, [prop]: event.target.value });
-    };
+  const handleChange = (prop) => (event) => {
+    setUserValues({ ...userValues, [prop]: event.target.value });
+  };
 
-    const handleClickShowPassword = () => {
-      setUserValues({ ...userValues, showPassword: !userValues.showPassword });
-    };
+  const handleClickShowPassword = () => {
+    setUserValues({ ...userValues, showPassword: !userValues.showPassword });
+  };
 
-    const handleClickShowConfPassword = () => {
-      setUserValues({ ...userValues, showConfPassword: !userValues.showConfPassword });
-    };
+  const handleClickShowConfPassword = () => {
+    setUserValues({ ...userValues, showConfPassword: !userValues.showConfPassword });
+  };
 
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-  // React.useEffect(() => {
-  //   setRefForm(
-  //     <></>
-  //   )
-  // }, [])
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   emailLinkComplete();
   return (
     <div className={classes.mainRoot}>
-      <div className="signupForm" style={{display: formDisabler}}>
-      <title>Registration</title>
+      <div className="signupForm" style={{ display: formDisabler }}>
+        <title>Registration</title>
         <form className={classes.root} autoComplete="off" onSubmit={UserRegisteration}>
           <FormControl>
             <InputLabel htmlFor="my-input">Username</InputLabel>
@@ -241,9 +234,10 @@ export default function Signup() {
         </form>
         <Link to="/Login" style={{ textDecoration: "none" }}>
           <Button size="small" type="button" variant="outlined" color="primary" style={{ border: "none" }}>Signin Instead</Button>
-        </Link>
+        </Link><br />
+        <span style={{ color: "red" }}>{lblError}</span>
       </div>
-      {refForm}
+      <span>{refForm}</span>
     </div>
   );
 }
